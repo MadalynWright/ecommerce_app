6115e3817e8222d0f46472504e55b64a200c5318
Madalyn Wright <mwright8168@gmail.com>
2016-10-19 14:59:15 -0400

    added some rspec tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+	validates :name, presence: true
+	validates :quantity, presence: true
+	validates :quantity, :numericality => { :greater_than_or_equal_to => 0 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "doesn't allow a product's name to be nil" do
+  	macbook = FactoryGirl.build(:product, name: nil)
+  	expect(macbook).to_not be_valid
+  end
+  it "doesn't allow a product's quantity to be nil" do
+  	macbook = FactoryGirl.build(:product, quantity: nil)
+  	expect(macbook).to_not be_valid
+  end
+    it "doesn't allow a product's quantity to be negative" do
+  	macbook = FactoryGirl.build(:product, quantity: -1)
+  	expect(macbook).to_not be_valid
+  end
 end
